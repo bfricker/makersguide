e0a26d1efb7fae262f4ff24c92e8aea1b2a8c3be
David <maker@sabertron.com>
2016-01-18 22:08:33 -0800

    Removed unused code

--- 14_lightsaber_sounds/src/lightsaber_effects_player.c
+++ 14_lightsaber_sounds/src/lightsaber_effects_player.c
@@ -1,19 +1,4 @@
-/**************************************************************************//**
- * @file
- * @brief Empty Project
- * @author Energy Micro AS
- * @version 3.20.2
- ******************************************************************************
- * @section License
- * <b>(C) Copyright 2014 Silicon Labs, http://www.silabs.com</b>
- *******************************************************************************
- *
- * This file is licensed under the Silicon Labs Software License Agreement. See 
- * "http://developer.silabs.com/legal/version/v11/Silicon_Labs_Software_License_Agreement.txt"  
- * for details. Before using this software for any purpose, you must agree to the 
- * terms of that agreement.
- *
- ******************************************************************************/
+
 #include "em_device.h"
 #include "em_chip.h"
 #include "em_cmu.h"
@@ -24,22 +9,6 @@
 #include <stdlib.h>
 #include "adxl.h"
 #include "utilities.h"
-
-#define FREE_FALL_INT_PIN	12
-#define ACTIVITY_INT_PIN	11
-
-// This will get called whenever the GPIO interrupt occurs
-// when we pass it into the set_gpio_interrupt function below
-int GPIO_int_callback(uint8_t pin)
-{
-	bool pin_state = GPIO_PinInGet(gpioPortB, pin);
-
-	// Set the appropriate LED based on the state of the INT1/INT2 pin
-	if (pin == ACTIVITY_INT_PIN) set_led(0,pin_state);
-	if (pin == FREE_FALL_INT_PIN) set_led(1,pin_state);
-
-	return 0;
-}
 
 int main(void)
 {
@@ -74,9 +43,6 @@ int main(void)
 	}
 
 	i2c_init_registers();
-
-	set_gpio_interrupt(gpioPortB,  ACTIVITY_INT_PIN, true, false, (GPIOINT_IrqCallbackPtr_t) GPIO_int_callback);
-	set_gpio_interrupt(gpioPortB,  FREE_FALL_INT_PIN, true, false, (GPIOINT_IrqCallbackPtr_t) GPIO_int_callback);
 
 	// We need to start the sound, which starts DMA, before we set up the DAC_TIMER
 	play_sound(SABER_IDLE);
